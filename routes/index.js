ea8202668a6aaa3d8e045501bc521354f5fb7881
Eren Yener <eren.yener@trendyol.com>
2017-11-06 21:07:32 +0300

    test

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var User = require('../models/user');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.layout('Layout', {title:"Scrum Poker Planning", centeredTitle:false}, {content:{block:"index", data:{title:"Matthew"}}}); 
+  res.layout('layout', {title:"Scrum Poker Planning", centeredTitle:false}, {content:{block:"index", data:{title:"Matthew"}}}); 
 });
 
 module.exports = router;
