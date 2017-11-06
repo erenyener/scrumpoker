360244e3f3f68499a150f495059654d7b899ad28
Eren Yener <eren.yener@trendyol.com>
2017-11-06 20:13:11 +0300

    test

--- app.js
+++ app.js
@@ -3,34 +3,13 @@ var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var ejsLayout = require('ejs-layouts');
-var mongoose = require('mongoose');
-
-//Mongo Connection
-
-var mongoDburi = 'mongodb://scrumpokerapp:PhP5Cpp23@ds243805.mlab.com:43805/scrumpokerdb';
-mongoose.connect(mongoDburi);
 
 
-//Routes
-var index = require('./routes/index');
 var testRoute = require('./routes/test-route');
-var scrumMasterPanel = require('./routes/scrum-master-panel.js');
-var voterLogin = require('./routes/voter-login.js');
-var voterVote = require('./routes/voter-vote.js');
-
-//api routes
-var sprints = require('./routes/sprints');
-var issues = require('./routes/issues');
-var votes = require('./routes/votes');
-var voters = require('./routes/voters');
 
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(ejsLayout.express);
 
 
 app.use(logger('dev'));
@@ -40,17 +19,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', index);
 app.use('/', testRoute);
 
-app.use('/smp', scrumMasterPanel);
-app.use('/vl', voterLogin);
-app.use('/vv', voterVote);
-
-app.use('/sprints', sprints);
-app.use('/issues', issues);
-app.use('/votes', votes);
-app.use('/voters', voters);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
