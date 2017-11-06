d2c18ec509c3fb6ec5fac34121cfebce213c0de3
Eren Yener <eren.yener@trendyol.com>
2017-11-06 20:46:36 +0300

    test

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+app.engine('ejs', require('ejs').renderFile);
 app.set('view engine', 'ejs');
 app.use(ejsLayout.express);
 
