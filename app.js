9ff2ff6ca88b3e1c9932d22ac8ea654b45618f05
Eren Yener <eren.yener@trendyol.com>
2017-11-06 21:00:52 +0300

    test

--- app.js
+++ app.js
@@ -39,7 +39,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname + '../public')));
 
 app.use('/', index);
 
